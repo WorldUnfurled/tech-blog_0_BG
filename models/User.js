57eb06f5820488d15f73febbc82cbb4f37d5053c
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-21 13:58:05 -0400

    Added user columns in User model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull:false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    }
+)
