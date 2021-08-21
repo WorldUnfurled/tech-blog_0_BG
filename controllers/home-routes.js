566fded63832f9c1ac8c494476c31efdc3c21c98
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-21 14:45:56 -0400

    Added route to display all posts in home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,32 @@ const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
 
 // display all posts
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        });
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+            
+            res.render('view-one', { post });
+        } else {
+            res.status(500).json(err);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // display single post
 router.get('/post/:id', async (req, res) => {});
