f93f9d75b54c0520680d5a15b6a3dc0df2ba11ff
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-21 16:33:52 -0400

    Added comment post route

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { Comment } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+
+    const createComment = await Comment.create({
+      content: req.body.content,
+      user_id: req.session.user_id
+    });
+
+    res.json(createComment);
+
+  } catch (err) {
+
+    res.status(500).json(err);
+    
+  }
+});
+
+module.exports = router;
