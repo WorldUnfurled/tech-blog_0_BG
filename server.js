e80369120080a98e76a7846a50cf6101ea647434
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-21 14:17:58 -0400

    Added session store, handlebars engine, and express use methods to server

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ const PORT = process.env.PORT || 3000;
 
 const sequelize = require('./config/connection');
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Zucc is a lizard',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 });
