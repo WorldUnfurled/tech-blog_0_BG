78be5738b7771fc9e84592547de58706fa6b95f4
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-21 14:49:22 -0400

    Added route to see single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,24 @@ const { Post, Comment, User } = require('../models/');
 // display all posts
 router.get('/', async (req, res) => {
     try {
+        const postData = await Post.findAll({
+          include: {
+              model: User,
+              attributes: ['username']
+          }
+        });
+    
+        const posts = postData.map(post => post.get({ plain: true }));
+    
+        res.render('view-all', { posts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// display single post
+router.get('/post/:id', async (req, res) => {
+    try {
         const postData = await Post.findByPk(req.params.id, {
             include: [
                 User,
@@ -28,9 +46,6 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
-
-// display single post
-router.get('/post/:id', async (req, res) => {});
 
 // login
 router.get('/login', async (req, res) => {});
